4ee7aea05cb7769c0e2a7538f3df70780c7f93fd
Noah <noah@persc.nl>
2017-07-01 01:01:58 +0200

    homepage - responsive

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,44 @@
+<footer>
+    <div class="container">
+        <div class="space-30"></div>
+        <div class="row">
+            <div class="col-md-6 ">
+                <ul>
+                    <li>
+                        Sitemap
+                        <div class="space-10"></div>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">Producten</a></li>
+                            <li><a href="">Installatie</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-md-push-3">
+                <ul>
+                    <li>
+                        Over Sluistechniek
+                        <div class="space-10"></div>
+                        <ul>
+                            <li>KVK: 2378238234</li>
+                            <li>BTW: 1212131313</li>
+                            <li>Tel: 32323232</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="space-30"></div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer-wrapper">
+                    <span>&#9400; {{date('Y')}} - <a href="{{url('/')}}"> Sluistechniek.nl </a></span></div>
+            </div>
+        </div>
+        <div class="space-30"></div>
+
+    </div>
+
+</footer>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,8 +3,8 @@
 @section('description','')
 
 @section('header')
-    <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/slick.css')}}"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('css/slick-theme.css')}}"/>
 @endsection
 @section('content')
 
@@ -40,42 +40,146 @@
     </div>
 
     <div class="space-100"></div>
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="owl-carousel owl-theme" id="home-carousel">
-                <div class="item">
-                    <img src="{{asset('images/camera2.png')}}" alt="">
-                    <span>Canon powershot SX620</span>
-                    <ul>
-                        <li>21,6 megapixel CMOS Sensor</li>
-                        <li>25x optische zoom</li>
-                        <li>Wifi en NRC</li>
-                    </ul>
-                    <p></p>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="slick" id="home-carousel">
+                    <div class="item">
+                        <img src="{{asset('images/camera2.png')}}" alt="">
+                        <div class="span-align">
+                            <span>Canon powershot SX620</span>
+                        </div>
+                        <ul>
+                            <li>21,6 megapixel CMOS Sensor</li>
+                            <li>25x optische zoom</li>
+                            <li>Wifi en NRC</li>
+                        </ul>
+                    </div>  <div class="item">
+                        <img src="{{asset('images/camera2.png')}}" alt="">
+                        <div class="span-align">
+                            <span>Canon powershot SX620</span>
+                        </div>
+                        <ul>
+                            <li>21,6 megapixel CMOS Sensor</li>
+                            <li>25x optische zoom</li>
+                            <li>Wifi en NRC</li>
+                        </ul>
+                    </div>  <div class="item">
+                        <img src="{{asset('images/camera2.png')}}" alt="">
+                        <div class="span-align">
+                            <span>Canon powershot SX620</span>
+                        </div>
+                        <ul>
+                            <li>21,6 megapixel CMOS Sensor</li>
+                            <li>25x optische zoom</li>
+                            <li>Wifi en NRC</li>
+                        </ul>
+                    </div>  <div class="item">
+                        <img src="{{asset('images/camera2.png')}}" alt="">
+                        <div class="span-align">
+                            <span>Canon powershot SX620</span>
+                        </div>
+                        <ul>
+                            <li>21,6 megapixel CMOS Sensor</li>
+                            <li>25x optische zoom</li>
+                            <li>Wifi en NRC</li>
+                        </ul>
+                    </div>
+                    <div class="item">
+                        <img src="{{asset('images/camera2.png')}}" alt="">
+                            <div class="span-align">
+                            <span>Canon powershot SX620</span>
+                            </div>
+                            <ul>
+                                <li>21,6 megapixel CMOS Sensor</li>
+                                <li>25x optische zoom</li>
+                                <li>Wifi en NRC</li>
+                            </ul>
+                        </div>
                 </div>
+
 
             </div>
         </div>
     </div>
+    <div class="clear-fix"></div>
+    <div class="space-100"></div>
+
+    <div class="container-fluid no-padding">
+        <div class="home-wrapper">
+            <div class="text-wrapper">
+                <div class="space-30"></div>
+                <div class="space-50"></div>
+                <h2>Bevelig uw huis met SluisTechniek</h2>
+                <p>Sluistechniek kan ook de installatie verzorgen</p>
+                <div class="space-50"></div>
+                <div class="space-30"></div>
+
+            </div>
+        </div>
+    </div>
+    <div class="space-100"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 home-installatie">
+                <h2>Installatie door Sluistechniek</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab culpa eius et exercitationem fugiat hic
+                    illum ipsa, itaque officia omnis, quam quia ratione suscipit velit voluptatem! Aliquid ipsam magni
+                    voluptate
+                    Lorem ipsum dolor sit amet <br> <br> consectetur adipisicing elit. Ab aliquid architecto consequatur
+                    consequuntur cumque delectus, hic id illo inventore, nihil nisi quidem quos ratione rerum sint ut
+                    vel voluptates voluptatibus.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores debitis dolores eligendi esse
+                    nam obcaecati omnis perspiciatis quia! Accusamus, animi consequatur delectus eaque ipsa numquam quae
+                    qui ratione sit velit?
+                </p>
+            </div>
+            <div class="col-md-4 col-md-offset-2">
+                <div class="space-70"></div>
+                <img src="{{asset('images/camera4.png')}}" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="space-100"></div>
+
 
 @endsection
 @section('scripts')
-    <script src="{{asset('js/owl.carousel.js')}}"></script>
+    <script src="{{asset('js/slick.min.js')}}"></script>
     <script>
-        $('.owl-carousel').owlCarousel({
-            loop: true,
-            margin: 10,
-            nav: true,
-            responsive: {
-                0: {
-                    items: 1
+        $('.slick').slick({
+            slidesToShow: 4,
+            dots: false,
+            infinite: true,
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 1340,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
                 },
-                600: {
-                    items: 3
+                {
+                    breakpoint: 995,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
                 },
-                1000: {
-                    items: 4
-                }
-            }
-        })</script>
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }],
+            prevArrow: "<div class='slick-left pull-left'><i class='glyphicon glyphicon-chevron-left' aria-hidden='true'></i></div>",
+            nextArrow: "<div class='slick-right pull-right'><i class='glyphicon glyphicon-chevron-right' aria-hidden='true'></i></div>"
+        });
+
+    </script>
 @endsection
