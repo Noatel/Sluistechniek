26579344d9a95aefdaeb6d4e1d6956fbfab3ddad
Noah <noah@persc.nl>
2017-07-12 11:50:26 +0200

    Push

--- resources/views/partials/seo.blade.php
+++ resources/views/partials/seo.blade.php
@@ -5,7 +5,7 @@
     <meta property="og:description" content="@yield('description', config('app.description'))"/>
     <meta property="og:site_name" content="{{config('app.name')}}"/>
     <meta property="og:image" content="@yield('image', asset('/images/seo.png'))"/>
-    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:card" content="summary"/>
     <meta name="twitter:title" content="@yield('title')Sluis Techniek"/>
     <meta name="twitter:description" content="@yield('description', config('app.description'))"/>
     <meta name="twitter:image" content="@yield('image', asset('/images/seo.png'))"/>
